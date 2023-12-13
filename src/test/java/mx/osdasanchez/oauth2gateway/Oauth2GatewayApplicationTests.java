f9970c7b2777985fdabb62f9de9f8c3ad960cf00
Oscar Daniel Sanchez Soto <osdasanchez@outlook.com>
2023-12-13 03:02:19 -0600

    first commit

---
+++ src/test/java/mx/osdasanchez/oauth2gateway/Oauth2GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.osdasanchez.oauth2gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
