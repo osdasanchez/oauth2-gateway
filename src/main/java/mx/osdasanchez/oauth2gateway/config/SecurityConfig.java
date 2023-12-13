cebb7cd578ce8b6a2bf165124f8d417f8a53a33a
Oscar Daniel Sanchez Soto <osdasanchez@outlook.com>
2023-12-13 03:35:33 -0600

    se agrega security config.

---
+++ src/main/java/mx/osdasanchez/oauth2gateway/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package mx.osdasanchez.oauth2gateway.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final ReactiveClientRegistrationRepository repository;
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(exchange -> exchange
+                        .pathMatchers("/**").permitAll()
+                        .anyExchange().authenticated())
+                .oauth2Login(Customizer.withDefaults())
+                .oauth2Client(Customizer.withDefaults())
+                .logout(logout -> logout.logoutUrl("/logout").logoutSuccessHandler(successHandler(repository)))
+                .build();
+    }
+
+    ServerLogoutSuccessHandler successHandler(ReactiveClientRegistrationRepository repository) {
+        OidcClientInitiatedServerLogoutSuccessHandler successHandler = new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+        successHandler.setPostLogoutRedirectUri("{baseUrl}/logged-out");
+        return successHandler;
+    }
+
+    /*
+     * INSERT INTO `client_post_logout_redirect_uris` (`client_id`, `post_logout_redirect_uris`) VALUES ('3', 'http://127.0.0.1:8081/logged-out')
+     * */
+}
